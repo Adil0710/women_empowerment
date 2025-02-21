d75a811cca42150a4f64ad83e0e7183b6de88b60
Adil0710 <padil2246@gmail.com>
2025-02-21 15:44:23 +0530

    second commit

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -5,7 +5,7 @@ import bcrypt from "bcryptjs";
 import dbConnect from "@/app/lib/dbConnect";
 import User from "@/app/models/User";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   try {
     await dbConnect();
     const { name, email, password, phone, emergencyContacts } = await req.json();
